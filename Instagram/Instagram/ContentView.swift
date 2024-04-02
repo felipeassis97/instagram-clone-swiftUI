9609ac0f8c65898aa4c8169eda56bac25696cba9
Felipe Assis <felipeassis97@gmail.com>
2024-04-02 09:17:53 -0300

    feat: Initial structure

---
+++ Instagram/Instagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Felipe Assis on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Felipe Assis on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
